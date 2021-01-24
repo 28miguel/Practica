9ca44b6e0b0dd8858a13e6a495162ca9d4dfb4ea
gurug <gurug@LAPTOP-1AJ53ABV>
2021-01-24 13:36:04 +0100

    protogemas que me voy a gastar en la 1.3

---
+++ src/CazadorDemonios.java
@@ -0,0 +1,9 @@
+
+public class CazadorDemonios {
+
+	public static void main(String[] args) {
+		int i=7+5;
+		System.out.println(i);
+	}
+
+}
--- src/RamaDos.java
+++ src/RamaDos.java
@@ -3,6 +3,7 @@ public class RamaDos {
 
 	public static void main(String[] args) {
 		System.out.println("rama2");
+		System.out.println("Me vofy a gastar mas de 30K de protogemas");
 	}
 
 }
